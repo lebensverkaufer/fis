a5fbf8e178689c25ccdebbb87d24ef12805a63e1
foxwhite <belgeomax@gmail.com>
2016-04-08 20:50:46 +0300

    interface structure init

---
+++ interface/main_wizard.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+
+import new_variable as varFrame
+import new_lTerm as ltFrame
+
+def mainFrame(appFrame):
+    mainWizard = tk.Frame(appFrame)
+    mainWizasrd.pack()
+    Label(mainWizard, text="here be main Wizard").pack(side=LEFT)
+    return mainWizard
--- main.py
+++ main.py
@@ -1,25 +1,47 @@
 import tkinter as tk
+from tkinter import *
+
+import interface.main_wizard as UI
 
 class Application(tk.Frame):
+
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
+        master.minsize(width=666, height=666)
+
+        # self.variablesInputGroup = LabelFrame(self, text="Новая переменная", padx=5, pady=5)
+        # self.showGroup_createNewVar(self.variablesInputGroup)
+
         self.pack()
-        self.createWidgets()
+        mainFrame = UI.mainFrame(self)
+        
+    # def showGroup_createNewVar(self, container):   
+        # container.pack(padx=10, pady=10)
 
-    def createWidgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+    #     varName = StringVar()
+    #     self.makeEntry(container, "Имя переменной", 10, textvariable = varName)
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                                            command=root.destroy)
-        self.QUIT.pack(side="bottom")
+    #     def callback():
+    #         print(varName.get())
 
-    def say_hi(self):
-        print("hi there, everyone!")
-        print ("hoa!")
+    #     createVarBtn = Button(self, text="Создать переменную", width=10, command=callback)
+    #     createVarBtn.pack()
+
+
+    # def makeEntry(self, parent, caption, width=None, **options):
+    #     Label(parent, text=caption).pack(side=LEFT)
+    #     entry = Entry(parent, **options)
+    #     if width:
+    #         entry.config(width=width)
+    #     entry.pack(side=LEFT)
+    #     return entry
+
+    # def createWidgets(self):
+    #     newVarBtn = Button(self, text="Добавить переменную", command=self.showGroup_createNewVar(self))
+    #     newVarBtn.pack()
+
 
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
+# 
